f965ac0175cf25bafe6d423aad9cf9ba808c8c66
Rui De Campos <ruida-si@student.42porto.com>
2025-05-01 14:19:55 +0100

    minor correction

--- src/redir.c
+++ src/redir.c
@@ -6,7 +6,7 @@
 /*   By: ruida-si <ruida-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 18:38:21 by ruida-si          #+#    #+#             */
-/*   Updated: 2025/05/01 12:58:50 by ruida-si         ###   ########.fr       */
+/*   Updated: 2025/05/01 13:03:47 by ruida-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,8 +64,6 @@ static int	execute_redir(char *s, char *file, t_mini *mini, int n)
 		return (0);
 	if (ft_strcmp(s, "<<"))
 		file = get_new_str(file);
-	else
-		file = ft_strdup(file);
 	fd = 0;
 	if (ft_strncmp(s, "<<", 2) == 0)
 	{
